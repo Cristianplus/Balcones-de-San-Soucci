2bec7b21845d34f9d615eccdda71836a4b40224e
Cristian Cordero <cristiancordero2810@gmail.com>
2025-04-12 03:12:16 -0500

    Creación y configuración de la base de datos, implementando las tablas en phpMyAdmin y algunos datos de ejemplo en cada tabla

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,21 @@
 // Conexión a la base de datos 
+
+<?php
+// Parámetros de conexión a la base de datos
+$host = "localhost";
+$usuario = "root";
+$contraseña = "cristian2910";
+$baseDeDatos = "condominio";
+
+// Crear conexión
+$conn = new mysqli($host, $usuario, $contraseña, $baseDeDatos);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Configurar el conjunto de caracteres
+$conn->set_charset("uft8");
+
+?>
